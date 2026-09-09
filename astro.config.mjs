7b0db9e324b1226d3b38107a3216b3d2aef07103
Hermes <augustherrstrom@gmail.com>
2026-09-09 21:33:07 +0200

    Fix asset paths for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://aggelito.github.io',
-  base: '/naprapatolof',
+  site: 'https://lancingnaprapati.se',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
